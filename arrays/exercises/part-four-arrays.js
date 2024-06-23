a3ba7ded7e7bcffb27090e81a4e6e1208d470bc1
jordan barry <jordanbarry@jordans-Mini.attlocal.net>
2024-06-23 13:19:41 -0500

    arraysPart4

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,18 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
-
+holdCabinet1. concat(holdCabinet2);
+console.log(holdCabinet1);
+    //The concat method doesn't alter the array.
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
-
+holdCabinet1.slice(1, 4)
+console.log(holdCabinet1);
+holdCabinet2.slice(2, 3);
+console.log(holdCabinet2);
+    //The slice method doesn't modify the original array.
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse();
+console.log(holdCabinet1);
+holdCabinet2.sort();
+console.log(holdCabinet2);
+    // The reverse method reverses the order the array. The sort method scrambles the order of an array. Both of these methods alter the original array.
