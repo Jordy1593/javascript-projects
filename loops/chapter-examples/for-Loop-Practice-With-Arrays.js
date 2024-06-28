79790f7b923d11511480d149bc4a6f5a8843b0f1
jordan barry <jordanbarry@jordans-Mini.attlocal.net>
2024-06-28 11:41:10 -0500

    update

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,6 +1,8 @@
 // create an array variable containing the names
-let newNames = ["Jordon","Justus","Jamela"]
+const input = require('readline-sync');
+let newNames = ["Jordon", "Justus", "Jamela"]
+let newNew = []
 // write a for loop that prints each name on a different line
 for (let i = 0; i < newNames.length; i++){
-    console.log(newNames[i]);
+    let newNew = input.question(newNames[i]);
 }
